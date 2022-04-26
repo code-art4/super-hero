00500cc24f7c9d2e25383076f598d779bcfc3d5e
code-art4 <ojotriumph4@gmail.com>
2022-04-26 09:44:41 +0100

    Added superhero details with without a database

---
+++ data/superHeroList.js
@@ -0,0 +1,104 @@
+export const superheroList = [
+    {
+        _id: 1,
+        name: "Superman",
+        category: "DC",
+        image: "https://c4.wallpaperflare.com/wallpaper/551/425/85/superman-dc-comics-movies-henry-cavill-wallpaper-preview.jpg"
+    },
+    {
+        _id: 2,
+        name: "Batman",
+        category: "DC",
+        image: "https://c4.wallpaperflare.com/wallpaper/314/216/1004/batman-digital-art-comics-artwork-wallpaper-preview.jpg"
+    },
+    {
+        _id: 3,
+        name: "Shazam",
+        category: "DC",
+        image: "https://c4.wallpaperflare.com/wallpaper/721/1013/127/comics-shazam-wallpaper-preview.jpg"
+    },
+    {
+        _id: 4,
+        name: "Wonder woman",
+        category: "DC",
+        image: "https://c4.wallpaperflare.com/wallpaper/74/892/916/dceu-comics-brunette-movies-wallpaper-preview.jpg"
+    },
+    {
+        _id: 5,
+        name: "Aquaman",
+        category: "DC",
+        image: "https://c4.wallpaperflare.com/wallpaper/423/134/523/aquaman-2018-movies-movies-hd-wallpaper-preview.jpg"
+    },
+    {
+        _id: 6,
+        name: "Ironman",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/35/536/33/iron-man-marvel-comics-superhero-tony-stark-wallpaper-preview.jpg"
+    },
+    {
+        _id: 7,
+        name: "Thor",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/70/1011/821/thor-fat-thor-marvel-cinematic-universe-hd-wallpaper-preview.jpg"
+    },
+    {
+        _id: 8,
+        name: "Black widow",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/934/547/670/scarlett-johansson-black-widow-iron-man-2-superheroines-wallpaper-preview.jpg"
+    },
+    {
+        _id: 9,
+        name: "Scarlett Witch",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/153/570/467/scarlett-johansson-infinity-vision-hulk-wallpaper-preview.jpg"
+    },
+    {
+        _id: 10,
+        name: "Dr. Strange",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/94/767/704/scarlett-johansson-infinity-vision-hulk-wallpaper-preview.jpg"
+    },
+    {
+        _id: 11,
+        name: "Black panther",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/810/82/503/5bd084c53fd86-wallpaper-preview.jpg"
+    },
+    {
+        _id: 12,
+        name: "Hulk",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/1015/886/533/hulk-the-avengers-marvel-cinematic-universe-wallpaper-preview.jpg"
+    },
+    {
+        _id: 13,
+        name: "Hawkeye",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/472/143/808/hawkeye-marvel-comics-the-avengers-jeremy-renner-wallpaper-preview.jpg"
+    },
+    {
+        _id: 14,
+        name: "Captain America",
+        category: "Marvel",
+        image: "https://c4.wallpaperflare.com/wallpaper/441/134/349/the-avengers-chris-evans-captain-america-avengers-age-of-ultron-wallpaper-preview.jpg"
+    },
+    {
+        _id: 15,
+        name: "Goku",
+        category: "Anime",
+        image: "https://c4.wallpaperflare.com/wallpaper/410/227/670/dragon-ball-super-son-goku-ultra-instinct-goku-kamehameha-wallpaper-preview.jpg"
+    },
+    {
+        _id: 16,
+        name: "Saitama",
+        category: "Anime",
+        image: "https://c4.wallpaperflare.com/wallpaper/608/190/519/one-punch-man-saitama-black-background-wallpaper-preview.jpg"
+    },
+    {
+        _id: 17,
+        name: "Sasuke",
+        category: "Anime",
+        image: "https://c4.wallpaperflare.com/wallpaper/370/127/644/uchiha-sasuke-rinnegan-eternal-mangekyou-sharingan-naruto-shippuuden-wallpaper-preview.jpg"
+    },
+]
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: { 
+        "logo": "My Soul, cursive", 
+        "Roboto": "Roboto, sans-serif", 
+        "whisper": "Whisper, cursive" 
+      },
+    },
+  },
+  plugins: [],
+};
